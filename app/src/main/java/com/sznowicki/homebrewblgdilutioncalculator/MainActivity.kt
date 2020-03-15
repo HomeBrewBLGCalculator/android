7360677a65888e5506d5c6acd83b1abe3d8eed49
Szymon Nowicki <sznowicki@gmail.com>
2020-03-15 23:28:25 +0100

    [wip]

--- app/src/main/java/com/sznowicki/homebrewblgdilutioncalculator/MainActivity.kt
+++ app/src/main/java/com/sznowicki/homebrewblgdilutioncalculator/MainActivity.kt
@@ -2,6 +2,9 @@ package com.sznowicki.homebrewblgdilutioncalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.TextView
 import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
@@ -9,6 +12,23 @@ class MainActivity : AppCompatActivity() {
     private lateinit var inputReceivedBlg: TextInputLayout
     private lateinit var inputWantedBlg: TextInputLayout
     private lateinit var inputWotVolume: TextInputLayout
+    private lateinit var textResult: TextView
+
+    private fun showResult() {
+        val receivedBlg = inputReceivedBlg.editText?.text.toString().toFloat()
+        val wantedBlg = inputWantedBlg.editText?.text.toString().toFloat()
+        val wotVolume = inputWotVolume.editText?.text.toString().toFloat()
+
+        if (receivedBlg < 1 || wantedBlg < 1 || wotVolume < 1) {
+            return
+        }
+
+        var result = ((receivedBlg - wantedBlg) * wotVolume) / wantedBlg
+
+        val resultText = getResources().getString(R.string.result, result)
+
+        textResult.setText(resultText)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,8 +37,45 @@ class MainActivity : AppCompatActivity() {
         inputReceivedBlg = findViewById(R.id.input_received_blg)
         inputWantedBlg = findViewById(R.id.input_wanted_blg)
         inputWotVolume = findViewById(R.id.input_wot_volume)
+        textResult = findViewById(R.id.result)
 
-        // TODO: Read value from text field
-        val receivedBlg = inputReceivedBlg.editText?.text.toString()
+        inputReceivedBlg.editText?.addTextChangedListener(object: TextWatcher {
+            override fun afterTextChanged(s: Editable) {}
+
+            override fun beforeTextChanged(s: CharSequence, start: Int,
+                                           count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int,
+                                       before: Int, count: Int) {
+                showResult()
+            }
+        })
+
+        inputWantedBlg.editText?.addTextChangedListener(object: TextWatcher {
+            override fun afterTextChanged(s: Editable) {}
+
+            override fun beforeTextChanged(s: CharSequence, start: Int,
+                                           count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int,
+                                       before: Int, count: Int) {
+                showResult()
+            }
+        })
+
+        inputWotVolume.editText?.addTextChangedListener(object: TextWatcher {
+            override fun afterTextChanged(s: Editable) {}
+
+            override fun beforeTextChanged(s: CharSequence, start: Int,
+                                           count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int,
+                                       before: Int, count: Int) {
+                showResult()
+            }
+        })
     }
 }
